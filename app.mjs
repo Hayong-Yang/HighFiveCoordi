a2f944a1e866b6b67f96fb9df438783f447a4297
Hayong Yang <ssk04022@gmail.com>
2025-05-12 15:34:15 +0900

    feat(main): 정적리소스 추가 [HF-4]

--- app.mjs
+++ app.mjs
@@ -1,29 +1,36 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.mjs';
-import productRoutes from './routes/productRoutes.mjs';
-import wishlistRoutes from './routes/wishlistRoutes.mjs';
-import adRoutes from './routes/adRoutes.mjs';
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./router/userRoutes.mjs";
+import productRoutes from "./router/productRoutes.mjs";
+import wishlistRoutes from "./router/wishlistRoutes.mjs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { config } from "./config.mjs";
 
 dotenv.config(); // .env 파일 로딩
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// 정적 라우터
+app.use(express.static(path.resolve(__dirname, "public")));
 
 // 미들웨어
 app.use(express.json()); // JSON 요청 처리
 app.use(express.urlencoded({ extended: true })); // 폼 데이터 처리
 
 // 라우터 연결
-app.use('/auth', userRoutes);
-app.use('/product', productRoutes);
-app.use('/wish', wishlistRoutes);
+app.use("/auth", userRoutes);
+app.use("/product", productRoutes);
+app.use("/wish", wishlistRoutes);
 
 // 기본 라우터
-app.get('/', (req, res) => {
-    res.send('Node.js 서버가 실행 중입니다!');
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/main.html"));
 });
 
 // 서버 시작
-app.listen(PORT, () => {
-    console.log(`S서버가 포트 ${PORT}에서 실행 중`)})
+app.listen(config.host.port, () => {
+  console.log(`S서버가 포트 ${config.host.port}에서 실행 중`);
+});
--- controller/userController.mjs
+++ controller/userController.mjs
@@ -1,6 +1,6 @@
-import * as bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-import db from '../db/database.mjs';
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import db from "../db/database.mjs";
 
 // 회원가입
 exports.register = async (req, res) => {
@@ -14,12 +14,14 @@ exports.register = async (req, res) => {
       [userid, hashedPw, name, gender, email, phone]
     );
 
-    res.status(201).json({ message: '회원가입 성공', userId: result.insertId });
+    res.status(201).json({ message: "회원가입 성공", userId: result.insertId });
   } catch (err) {
-    if (err.code === 'ER_DUP_ENTRY') {
-      res.status(400).json({ message: '이미 존재하는 아이디 또는 이메일입니다.' });
+    if (err.code === "ER_DUP_ENTRY") {
+      res
+        .status(400)
+        .json({ message: "이미 존재하는 아이디 또는 이메일입니다." });
     } else {
-      res.status(500).json({ message: '서버 오류', error: err.message });
+      res.status(500).json({ message: "서버 오류", error: err.message });
     }
   }
 };
@@ -28,33 +30,49 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   const { userid, userpw } = req.body;
   try {
-    const [rows] = await db.execute(`SELECT * FROM user WHERE userid = ?`, [userid]);
+    const [rows] = await db.execute(`SELECT * FROM user WHERE userid = ?`, [
+      userid,
+    ]);
     const user = rows[0];
 
-    if (!user) return res.status(401).json({ message: '아이디 또는 비밀번호가 올바르지 않습니다.' });
+    if (!user)
+      return res
+        .status(401)
+        .json({ message: "아이디 또는 비밀번호가 올바르지 않습니다." });
 
     const isMatch = await bcrypt.compare(userpw, user.userpw);
-    if (!isMatch) return res.status(401).json({ message: '아이디 또는 비밀번호가 올바르지 않습니다.' });
+    if (!isMatch)
+      return res
+        .status(401)
+        .json({ message: "아이디 또는 비밀번호가 올바르지 않습니다." });
 
-    const token = jwt.sign({ id: user.idx, userid: user.userid }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
-    });
+    const token = jwt.sign(
+      { id: user.idx, userid: user.userid },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
 
-    res.json({ message: '로그인 성공', token });
-    } catch (err) {
-    res.status(500).json({ message: '서버 오류', error: err.message });
-    }
+    res.json({ message: "로그인 성공", token });
+  } catch (err) {
+    res.status(500).json({ message: "서버 오류", error: err.message });
+  }
 };
 
 // 유저 정보 조회
 exports.getProfile = async (req, res) => {
   const userId = req.user.id; // 토큰에서 가져온 사용자 ID
-    try {
-    const [rows] = await db.execute(`SELECT idx, userid, name, gender, email, phone, address, regdate FROM user WHERE idx = ?`, [userId]);
-    if (rows.length === 0) return res.status(404).json({ message: '사용자 정보가 없습니다.' });
+  try {
+    const [rows] = await db.execute(
+      `SELECT idx, userid, name, gender, email, phone, address, regdate FROM user WHERE idx = ?`,
+      [userId]
+    );
+    if (rows.length === 0)
+      return res.status(404).json({ message: "사용자 정보가 없습니다." });
 
     res.json(rows[0]);
-    } catch (err) {
-    res.status(500).json({ message: '서버 오류', error: err.message });
-    }
+  } catch (err) {
+    res.status(500).json({ message: "서버 오류", error: err.message });
+  }
 };
