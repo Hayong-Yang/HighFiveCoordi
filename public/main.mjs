513029857d4b72f1dd9b7a8552f417e85dbdcd0d
고현서 <hyeonseogo01@gmail.com>
2025-05-13 19:48:37 +0900

    feat(logIn):SCRUM-7 로그인, 로그아웃 [HF-10]

--- public/main.mjs
+++ public/main.mjs
@@ -1,15 +1,22 @@
 // 회원가입 창으로 이동
 document.getElementById("signUp").addEventListener("click", () => {
-    window.location.href = "/auth/signUp";
+  window.location.href = "/auth/signUp";
 });
 
 // 메인 > 로그인 이동
 document.getElementById("logIn").addEventListener("click", () => {
-    window.location.href = "/auth/logIn";
+  window.location.href = "/auth/logIn";
 });
 
 // 메인 > 위시리스트 이동
 document.getElementById("Wishlist").addEventListener("click", (event) => {
-    event.preventDefault();
-    window.location.href = "/auth/Wishlist";
+  event.preventDefault();
+  window.location.href = "/auth/Wishlist";
+});
+
+document.getElementById("logOut").addEventListener("click", function (e) {
+  e.preventDefault();
+  localStorage.removeItem("token");
+  alert("로그아웃 되었습니다.");
+  window.location.href = "/"; // 서버에 요청해서 메인 페이지로 리디렉션
 });
--- public/signup.mjs
+++ public/signup.mjs
@@ -66,3 +66,10 @@ document.getElementById("idCheck").addEventListener("click", async function () {
     console.log(hiddenIdCheck.value);
   }
 });
+
+document.getElementById("logOut").addEventListener("click", function (e) {
+  e.preventDefault();
+  localStorage.removeItem("token");
+  alert("로그아웃 되었습니다.");
+  window.location.href = "/"; // 서버에 요청해서 메인 페이지로 리디렉션
+});
