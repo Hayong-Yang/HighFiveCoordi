f1957034dd9cd05596c66d70fe48881c7d8194cf
Hayong Yang <ssk04022@gmail.com>
2025-05-22 20:42:56 +0900

    feat(setting) 유사색상 추천 기능으로만 바꿔둠.

--- data/colorHarmony.mjs
+++ data/colorHarmony.mjs
@@ -150,10 +150,15 @@ export async function getRecommendations(pickedColor, level) {
     recommendStrategies[Math.floor(Math.random() * recommendStrategies.length)];
 
   // 랜덤 전략 실행 → [{ category, hue }, ...]
-  const hueTargets = randomStrategy(baseColor);
+  //   const hueTargets = randomStrategy(baseColor);
+  //*********************************************************************
+  // similarHarmony만 수행하도록 잠시 바꿔둠!!!
+  //*********************************************************************
+  const hueTargets = similarHarmony(baseColor);
 
   console.log("베이스컬러", baseColor);
-  console.log(randomStrategy);
+  //   console.log(randomStrategy);
+  console.log(similarHarmony);
   console.log("hueTargets:", hueTargets);
   //   hueTargets.forEach((t) =>
   //     console.log(`[${t.category}] hue=${t.hue} (${typeof t.hue})`)
